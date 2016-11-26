bcf9bba1dc733087d2cec90d11bd0fdfa59c3e2b
Morgaroth <mateuszjaje@gmail.com>
2016-11-26 22:21:14 +0100

    improved testing

--- src/main/scala/io/github/morgaroth/quide/tests/TimeTest.scala
+++ src/main/scala/io/github/morgaroth/quide/tests/TimeTest.scala
@@ -1,11 +1,10 @@
 package io.github.morgaroth.quide.tests
 
-import java.io.{File => JFile}
+//import java.io.{File => JFile}
 
 import akka.actor.{ActorSystem, Props}
 import akka.event.Logging
 import akka.util.Timeout
-import better.files.Cmds._
 import io.github.morgaroth.quide.core.model.QValue
 import io.github.morgaroth.quide.core.model.gates.X
 import io.github.morgaroth.quide.core.register.custom_map.RegisterCustomMap
@@ -33,75 +32,71 @@ object TimeTest extends TestHelpers {
     "io.github.morgaroth.quide.core.register.sync.RegisterSync" -> RegisterSync.props _
   )
 
-  def doTest(registerName: String, registerSize: Int): Double = {
+  def doTest(registerName: String, registerSize: Int) {
     def saveVal(name: String, value: Double) = saveValue(name, registerName, registerSize, value)
 
     implicit val as = ActorSystem("timetest")
-    //    val times: Long = 1 to 5 map { _ =>
-    val times = {
-      val reg = RegisterActions(as.actorOf(registers(registerName)(registerSize)), registerSize)
-      val log = Logging(as, "test")
-      log.warning("start")
+    val reg = RegisterActions(as.actorOf(registers(registerName)(registerSize)), registerSize)
+    val log = Logging(as, "test")
+    log.warning("start")
+    val start = Platform.currentTime
+    val problemSize = registerSize - 1
+
+    val rounds = (math.Pi / 4 * registerSize).toInt
+    //      val rounds = problemSize * 2
+    val oracledValue = 2
+    log.warning(s"grovering $oracledValue using $rounds rounds for size $registerSize")
+    reg.run(X, 0)
+    reg.runWalsh()
+    //      val walshed = getValue(reg)
+    //      val walshedTime = System.currentTimeMillis()
+    println(getValueFrom(reg).toList.sortBy(_._2.modulus).takeRight(4).map(x => x._1 -> x._2.asString))
+
+    //      (0 to rounds toList) foreach { _ =>
+    //        reg.runOracle(oracledValue)
+    //        reg.runInversion()
+    //        println(getValueFrom(reg).toList.sortBy(_._2.modulus).takeRight(4).toString)
+    //      }
+
+    //      (0 to rounds toList) foreach { _ =>
+    var end = true
+    var roundsEffecctive = 0
+    while (end) {
       val start = Platform.currentTime
-      val problemSize = registerSize - 1
-
-      val rounds = (math.Pi / 4 * registerSize).toInt
-      //      val rounds = problemSize * 2
-      val oracledValue = 2
-      log.warning(s"grovering $oracledValue using $rounds rounds for size $registerSize")
-      reg.run(X, 0)
-      reg.runWalsh()
-      //      val walshed = getValue(reg)
-      //      val walshedTime = System.currentTimeMillis()
-      println(getValueFrom(reg).toList.sortBy(_._2.modulus).takeRight(4).map(x => x._1 -> x._2.asString))
-
-      //      (0 to rounds toList) foreach { _ =>
-      //        reg.runOracle(oracledValue)
-      //        reg.runInversion()
-      //        println(getValueFrom(reg).toList.sortBy(_._2.modulus).takeRight(4).toString)
-      //      }
-
-      //      (0 to rounds toList) foreach { _ =>
-      var end = true
-      var roundsEffecctive = 0
-      while (end) {
-        val start = Platform.currentTime
-        roundsEffecctive += 1
-        reg.runOracle(oracledValue)
-        reg.runInversion()
-        val values = getValueFrom(reg).toList.sortBy(_._2.modulus)
-        saveVal("round-time", Platform.currentTime - start)
-        if (values.takeRight(2).map(_._2.modulus).map(x => x * x).sum > 0.97) {
-          log.warning(s"End, effective rounds $roundsEffecctive for problem qbits $problemSize")
-          saveVal("effective-rounds", roundsEffecctive)
-          end = false
-        }
-        println(values.takeRight(4).map(x => (x._1, x._2.toString())))
+      roundsEffecctive += 1
+      reg.runOracle(oracledValue)
+      reg.runInversion()
+      val values = getValueFrom(reg).toList.sortBy(_._2.modulus)
+      saveVal("round-time", Platform.currentTime - start)
+      if (values.takeRight(2).map(_._2.modulus).map(x => x * x).sum > 0.97) {
+        log.warning(s"End, effective rounds $roundsEffecctive for problem qbits $problemSize")
+        saveVal("effective-rounds", roundsEffecctive)
+        end = false
       }
-
-      val values: List[(String, QValue, Double)] = getValueFrom(reg).toList.sortBy(_._2.modulus).map(x => (x._1, x._2, (x._2.modulus * x._2.modulus * 10000).toInt / 100.0))
-      //      values.foreach(println)
-      val propSum = values.map(x => x._2.modulus * x._2.modulus).sum
-      log.warning(s"${values.takeRight(5).toString} results ${values.size}, propSum $propSum")
-      val time = Platform.currentTime - start
-      log.error("stopping this shit")
-      //      log.error(s"stopping ${Await.result(gracefulStop(reg.reg, 4 minutes), 5 minutes)}")
-      as.stop(reg.reg)
-      time
-      //    } sum
+      println(values.takeRight(4).map(x => (x._1, x._2.toString())))
     }
 
+    val values: List[(String, QValue, Double)] = getValueFrom(reg).toList.sortBy(_._2.modulus).map(x => (x._1, x._2, (x._2.modulus * x._2.modulus * 10000).toInt / 100.0))
+    //      values.foreach(println)
+    val propSum = values.map(x => x._2.modulus * x._2.modulus).sum
+    log.warning(s"${values.takeRight(5).toString} results ${values.size}, propSum $propSum")
+    val time = Platform.currentTime - start
+    log.error("stopping this shit")
+    //      log.error(s"stopping ${Await.result(gracefulStop(reg.reg, 4 minutes), 5 minutes)}")
+    as.stop(reg.reg)
+    saveVal("execution-time", time)
     Await.result(as.terminate(), 1 minute)
     //    times * 1.0 / 5
-    times
   }
 
   def main(args: Array[String]) {
-    println(args.toList)
     val registerName = args.head
     val sizes: Iterable[Int] = if (args.tail.nonEmpty) args.tail.map(_.toInt) else 15 to 15 toList
-    val results = sizes map { size =>
-      size -> doTest(registerName, size)
+
+    1 to 5 map { _ =>
+      sizes map { size =>
+        size -> doTest(registerName, size)
+      }
     }
   }
 }
